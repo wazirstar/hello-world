9d61da50614408e5bf24043b5ed664a9b0c7bfee
Gayathri8187 <67357392+Gayathri8187@users.noreply.github.com>
2020-07-10 12:10:58 +0530

    Add files via upload

---
+++ BasicCalculatorTest.java
@@ -0,0 +1,23 @@
+package com.cts.training;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BasicCalculatorTest {
+
+	@Test
+	public void addTest() {
+		BasicCalculator calc  = new BasicCalculator();
+		//assertEquals(10, calc.add(5, 5));
+		assertEquals(10, calc.add(5, 5));
+		
+	}
+	@Test
+	public void subTest() {
+		BasicCalculator calc  = new BasicCalculator();
+		//assertEquals(5, calc.sub(10, 5));
+		assertEquals(0, calc.sub(5, 5));
+	}
+
+}
